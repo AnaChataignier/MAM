a0c5ef803801510026c4f7dfdbea7c2bb1f16cf3
carloshtbraga <carloshtbraga@gmail.com>
2023-11-07 02:16:56 -0300

    fix foto perfil delete qdo chega uma nova

--- authentication/models.py
+++ authentication/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import MinValueValidator, MinLengthValidator
 from .constants import STATE_CHOICES
-
+from django.core.files.storage import default_storage
 
 
 class Endereco(models.Model):
@@ -20,7 +20,29 @@ class Endereco(models.Model):
 
 class CustomUser(AbstractUser):
     telefone = models.CharField(max_length=15, null=False)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, default=None)
+    endereco = models.ForeignKey(
+        Endereco, on_delete=models.CASCADE, null=True, default=None
+    )
     foto = models.ImageField(upload_to="img/perfil_user/", null=True, blank=True)
 
+    def cliente_profile_filename(instance, filename):
+        ext = filename.split(".")[-1]
+        return f"{instance.first_name}_{instance.last_name}_profile.{ext}"
 
+    def save(self, *args, **kwargs):
+        if self.pk:  # Verifique se o usuário já existe no banco de dados
+            try:
+                # Obtém a instância atual do usuário no banco de dados
+                old_user = CustomUser.objects.get(pk=self.pk)
+                if old_user.foto and self.foto != old_user.foto:
+                    # Se a instância atual tiver uma foto diferente da anterior
+                    # exclua a foto anterior
+                    default_storage.delete(old_user.foto.name)
+            except CustomUser.DoesNotExist:
+                pass
+
+        # Chama a função que define o caminho da nova imagem
+        if self.foto:
+            self.foto.name = self.cliente_profile_filename(self.foto.name)
+
+        super(CustomUser, self).save(*args, **kwargs)
